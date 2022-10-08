22fd72435806f5d6b008f98150554d0f78db8dba
aeromandy <aeromandy2016@gmail.com>
2022-10-08 17:04:05 -0300

    typescript primeiros comandos

--- teste.js
+++
@@ -1,9 +0,0 @@
-let n1 = randon
-n2 = prompt('Digite um número')
-if (n1 > n2) {
-    alert('Esse número é maior')
-} else if (n1 < n2) {
-    alert('Esse número é menor')
-} else {
-    alert('Vc acertou!!')
-}
